7b6f3ee7ba782bebd259545cea283adb38dc7fcd
Braydon McAllister <45909931+BraydonMcAllister@users.noreply.github.com>
2022-07-29 10:58:46 -0400

    Update databaseconnection.php

--- php_one/php/databaseconnection.php
+++ php_one/php/databaseconnection.php
@@ -9,7 +9,7 @@ $charset = 'utf8mb4';
 
 
 $username = "testuser";
-$password = "Qwerter34!";
+$password = "";
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
